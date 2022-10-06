17fb26e2e018d09c24b13c07b14b65bd579fdbd0
heritier24 <heritier.ganza@kudibooks.com>
2022-10-06 16:35:52 +0200

    created clients controller

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientsController extends Controller
+{
+    
+    public function clients() 
+    {
+
+    }
+
+    public function clientById(int $id)
+    {
+
+    }
+
+    public function postClient() 
+    {
+
+    }
+
+    public function updateClient(int $id) 
+    {
+
+    }
+
+    
+}
