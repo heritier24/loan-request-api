45202b470e03c5df09f8d93ccb3947dad638d070
heritier24 <heritier.ganza@kudibooks.com>
2022-10-06 16:36:30 +0200

    created items controller

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemsController extends Controller
+{
+    
+    public function listItems()
+    {
+
+    }
+
+    public function postItem()
+    {
+
+    }
+
+    public function updateItem(int $id)
+    {
+
+    }
+    public function deleteItem(int $id) {
+
+    }
+
+    
+}
