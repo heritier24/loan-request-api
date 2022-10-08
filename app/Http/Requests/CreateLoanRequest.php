f0c73042eb445f68e051a170ec65aa45ae68b8c1
heritier24 <heritier.ganza@kudibooks.com>
2022-10-08 14:08:43 +0200

    create loan request form request success

---
+++ app/Http/Requests/CreateLoanRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class CreateLoanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "clientID" => "required|integer",
+            "itemID" => "required|integer",
+            "note",
+            "status"
+        ];
+    }
+
+      /**
+     * Handle a failed validation attempt.
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(response()
+                ->json(
+                    [
+                        "errors" => $validator->errors()->all()
+                    ],
+                    Response::HTTP_UNPROCESSABLE_ENTITY
+                ));
+        }
+    }
+
+}
